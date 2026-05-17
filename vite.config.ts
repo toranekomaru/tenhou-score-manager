713f91f37a8dc14bfdc812b4c4332af099324e9c
toranekomaru <2013tsyopon@gmail.com>
2026-05-17 16:12:33 +0900

    chore: regenerate node_modules folder to include missing dependency files

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : './',
+  base: './',
   plugins: [
     tailwindcss(),
     react(),
